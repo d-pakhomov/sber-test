12674c71cc305b8aaec7c4c1f98e9c649b2eb007
D. Pakhomov <d.v.pakhomov@gmail.com>
2020-09-10 09:57:51 +0300

    Инициализация репозитория

---
+++ src/com/company/Element.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Element {
+    public int x;
+    public int y;
+
+    public Element(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,105 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+
+    public static int MATRIX_HEIGHT = 10;
+    public static int MATRIX_WIDTH = 15;
+
+    public static void main(String[] args) {
+        Element[][] matrix = new Element[MATRIX_HEIGHT][MATRIX_WIDTH];
+
+        // Заполняем матрицу
+        for (int y = 0; y < MATRIX_HEIGHT; y++) {
+            for (int x = 0; x < MATRIX_WIDTH; x++) {
+                matrix[y][x] = new Element(x, y);
+            }
+        }
+
+        print(matrix);
+
+        // Генерируем "иголку", которую будет искать
+        Element needle = generateNeedle();
+        System.out.printf("\nИскомый элемент = (%d;%d)\n", needle.x, needle.y);
+
+        // Ищем соседей
+        ArrayList<Element> neighbors = search(needle, matrix);
+
+        if (neighbors == null) {
+            System.out.println("\nСоседей нет");
+            return;
+        }
+
+        System.out.print("\nСоседи: ");
+        for (Element item : neighbors) {
+            System.out.printf("(%d;%d)", item.x, item.y);
+        }
+    }
+
+    /**
+     * Печать матрицы в консоль
+     *
+     * @param matrix Матрица
+     */
+    private static void print(Element[][] matrix) {
+        for (int i = 0; i < MATRIX_HEIGHT; i++) {
+            for (int j = 0; j < MATRIX_WIDTH; j++) {
+                Element current = matrix[i][j];
+                System.out.printf("(%d;%d) ", current.x, current.y);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Сгенерировать рандомную "иголку" для поиска
+     *
+     * @return Элемент, выступающий в качестве искомой иголки
+     */
+    private static Element generateNeedle() {
+        int needleX = ThreadLocalRandom.current().nextInt(0, MATRIX_WIDTH);
+        int needleY = ThreadLocalRandom.current().nextInt(0, MATRIX_HEIGHT);
+
+        return new Element(needleX, needleY);
+    }
+
+    /**
+     * Поиск соседей элемента needle в матрице
+     *
+     * @param needle Искомый элемент
+     * @param source Матрица
+     * @return Список соседей
+     */
+    private static ArrayList<Element> search(Element needle, Element[][] source) {
+        if (needle.x >= MATRIX_WIDTH || needle.x < 0) {
+            return null;
+        }
+
+        if (needle.y >= MATRIX_HEIGHT || needle.y < 0) {
+            return null;
+        }
+
+        // Находим верхнуюю и нижнюю границы искомой области соседей
+        int upperBoundY = needle.y > 0 ? needle.y - 1 : 0;
+        int bottomBoundY = needle.y < MATRIX_HEIGHT - 1 ? needle.y + 1 : MATRIX_HEIGHT;
+
+        // Находим левую и правую границы искомой области соседей
+        int leftBoundX = needle.x > 0 ? needle.x - 1 : 0;
+        int rightBoundX = needle.x < MATRIX_WIDTH - 1 ? needle.x + 1 : MATRIX_WIDTH;
+
+        // Все элементы внутри этой области (за исключением "иголки") - искомые соседи
+        ArrayList<Element> result = new ArrayList<>();
+        for (int y = upperBoundY; y <= bottomBoundY; y++) {
+            for (int x = leftBoundX; x <= rightBoundX; x++) {
+                if (y == needle.y && x == needle.x) {
+                    continue;
+                }
+                result.add(source[y][x]);
+            }
+        }
+
+        return result;
+    }
+}
